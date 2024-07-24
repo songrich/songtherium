c7dcb7da1c099f74650d931bd60ce8a93dde4daa
songrich <minsu72110@gmail.com>
2024-07-24 16:47:13 +0900

    함수코드 미완료입니다. 코드 작성하지 못했습니다.

---
+++ 04.js
@@ -0,0 +1,10 @@
+// 문제 2번의 사자 객체를 가져옵니다.
+// const lion = { ... }
+const lion={이름:'심바',나이:3,주인공:'참',대사:'하쿠나마타타'}
+// 나이를 추가하는 함수입니다.
+function incrementAge(lion) {
+   return lion()  
+  }
+  
+  // 함수 실행 이후 사자의 현재 나이를 출력합니다.
+  console.log(incrementAge(lion))
